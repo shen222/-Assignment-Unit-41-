8180bcd3ef747403ead58b06131023d56b0241b5
shenliyanage <71073841+shen222@users.noreply.github.com>
2022-01-07 21:34:31 +0530

    redo on contact page , themes and quiz

---
+++ assets/js/questions.js
@@ -0,0 +1,127 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "which road is amdt located?",
+    answer: "galle road",
+    options: [
+      "mathara roadr",
+      "galle road",
+      "colombo",
+      "hambantota"
+    ]
+  },
+    {
+    numb: 2,
+    question: "when was amdt found?",
+    answer: "2005",
+    options: [
+      "2015",
+      "2010",
+      "2000",
+      "2005"
+    ]
+  },
+    {
+    numb: 3,
+    question: "name one of the 4 colors in the amdt logo?",
+    answer: "red,blue,yellow and green",
+    options: [
+      "red,blue,yellow and green",
+      "red,white,yellow and green",
+      "red,blue,yellow and black",
+      "red,purple,yellow and green"
+    ]
+  },
+    {
+    numb: 4,
+    question: "what hastag does amdt use?",
+    answer: "#weareamdt",
+    options: [
+      "#Sthy",
+      "#Amdt",
+      "#weeamdt",
+      "#weareamdt"
+    ]
+  },
+  {
+    numb: 5,
+    question: "how many degree courses does amdt offer?",
+    answer: "9 courses",
+    options: [
+      "12 courses",
+      "4 courses",
+      "9 courses",
+      "5 courses"
+    ]
+  },
+  {
+    numb: 6,
+    question: "how many social media channels does amdt use?",
+    answer: "4 social channels",
+    options: [
+      "4 social channels",
+      "8 social channels",
+      "12 social channels",
+      "9 social channels"
+    ]
+  },
+  {
+    numb: 7,
+    question: "amdt is partnered with which university from the uk?",
+    answer: "Falmouth university",
+    options: [
+      "kembridge",
+      "nibm",
+      "nsbm",
+      "Falmouth university"
+    ]
+  },
+  {
+    numb: 8,
+    question: "how old is amdt now?",
+    answer: "16",
+    options: [
+      "31",
+      "18",
+      "52",
+      "16"
+    ]
+  },
+  {
+    numb: 9,
+    question: "what was the event that amdt did on january 26th of 2020?",
+    answer: "premier league",
+    options: [
+      "exhibition",
+      "ceremony",
+      "party",
+      "premier league"
+    ]
+  },
+  {
+    numb: 10,
+    question: "does amdt offer a foundation course?",
+    answer: "yes",
+    options: [
+      "maybe",
+      "not yet",
+      "no",
+      "yes"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
